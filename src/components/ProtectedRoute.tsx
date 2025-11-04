5f2ad46aba4fbb0b0876f15af294289a429b0ed4
Subash <subashgopi2006@gmail.com>
2025-11-04 23:17:48 +0530

    altered

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/use-auth";
 
+
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
   const { user, loading } = useAuth();
   if (loading) return null;
